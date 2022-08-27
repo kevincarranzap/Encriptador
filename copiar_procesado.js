d300de7a4ac1e90137040477764d0de66e01e3f7
Kevin Carranza <kjcarranzap@gmail.com>
2022-08-27 13:05:18 -0500

    se implementa función copiar

---
+++ copiar_procesado.js
@@ -0,0 +1,5 @@
+var botonCopiar = document.querySelector("#boton_copiar");
+
+botonCopiar.addEventListener("click", function(){
+    navigator.clipboard.writeText(document.querySelector("#texto_procesado").textContent);
+});
