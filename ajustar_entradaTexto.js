aec8e25f239c4c4f79d2254b43a43d3240615cb6
Kevin Carranza <kjcarranzap@gmail.com>
2022-08-27 13:26:39 -0500

    se restablece cuadro de texto cuando está vacio

--- ajustar_entradaTexto.js
+++ ajustar_entradaTexto.js
@@ -3,4 +3,14 @@ var entradaTexto = document.querySelector("#entrada");
 entradaTexto.addEventListener("input", function(){
     entradaTexto.style.height = "1px";
     entradaTexto.style.height = (entradaTexto.scrollHeight)+"px";
+
+    if(entradaTexto.value == ""){
+        document.querySelector("#entrada").style.height = "235px";
+        document.querySelector("#titulo_contenedor_procesado").style.display = "inline";
+        document.querySelector("#mensaje_contenedor_procesado").style.display = "inline";
+        document.querySelector("#texto_procesado").style.display = "none";
+        document.querySelector("#boton_copiar").style.display = "none";
+        document.querySelector(".contenedor_procesado").style.width = "327px";
+        document.querySelector(".contenedor_procesado").style.gap = "16px";
+    }
 })
